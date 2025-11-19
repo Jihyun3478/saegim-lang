99eda95bfe5465e2bee049fe1f8bbee79a9985a8
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-19 19:57:55 +0900

    feat: 토큰 타입 정의

---
+++ token/token.go
@@ -0,0 +1,22 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	IDENT = "IDENT"
+	INT = "INT"
+
+	ASSIGN = "="
+	COMMA = ","
+	SEMICOLON = ";"
+	
+	VARIABLE = "변수"
+)
